aeee4c52fdd711ceb9c821a2ce3cb0ae7dbb6c6b
Waheal <2035582067@qq.com>
2025-04-06 23:33:41 +0800

    http模块优化

--- MSLX.Core/Utils/HttpService.cs
+++ MSLX.Core/Utils/HttpService.cs
@@ -1,11 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -53,30 +51,27 @@ namespace MSLX.Core.Utils
             return getResponse;
         }
 
-        public enum PostContentType { Json, FormUrlEncoded, MultipartFormData }
         public async static Task<HttpResponse> PostAsync(string path, PostContentType postContentType, object data)
         {
             using var service = new HttpService();
             service.SetDefaultHeadersUA(UAManager.GetUA(UAManager.UAType.MSLX));
-
             var postRequest = new HttpRequest
             {
                 Url = "https://api.mslmc.cn/v3" + path,
                 Method = HttpMethod.Post
             };
-
             /*
             // POST JSON示例
             var postRequest = new HttpService.HttpRequest
             {
-                ContentType = "application/json",
+                ContentType=PostContentType.Json,
                 Data = new { Name = "Test", Value = 123 }
             };
 
             // POST 表单示例
             var formRequest = new HttpService.HttpRequest
             {
-                ContentType = "application/x-www-form-urlencoded",
+                ContentType = PostContentType.FormUrlEncoded,
                 Data = new Dictionary<string, string>
                 {
                     ["username"] = "test",
@@ -84,33 +79,22 @@ namespace MSLX.Core.Utils
                 }
             };
 
-            // 文件上传示例
-            var fileContent = new FileContent(
-                File.OpenRead("test.jpg"),
-                "test.jpg",
-                "image/jpeg");
-
-            var multipartRequest = new HttpService.HttpRequest
+            // POST TEXT示例
+            var textRequest = new HttpService.HttpRequest
             {
-                ContentType = "multipart/form-data",
-                Data = new Dictionary<string, object>
-                {
-                    ["file"] = fileContent,
-                    ["description"] = "Sample file"
-                }
+                ContentType = PostContentType.Text,
+                Data = "123123"
+            };
+
+            // POST OCTET示例
+            var octetRequest = new HttpService.HttpRequest
+            {
+                ContentType = PostContentType.Octet,
+                Data = new byte[] { 0x01, 0x02, 0x03 }
             };
             */
-
-            if (postContentType == PostContentType.Json)
-            {
-                postRequest.ContentType = "application/json";
-                postRequest.Data = data;
-            }
-            else if (postContentType == PostContentType.FormUrlEncoded)
-            {
-                postRequest.ContentType = "application/x-www-form-urlencoded";
-                postRequest.Data = data;
-            }
+            postRequest.ContentType = postContentType;
+            postRequest.Data = data;
             var postResponse = await service.SendAsync(postRequest);
             service.Dispose();
             return postResponse;
@@ -147,13 +131,14 @@ namespace MSLX.Core.Utils
             }
         }
 
+        public enum PostContentType { Json, FormUrlEncoded, Text, Octet }
         public class HttpRequest
         {
             public required string Url { get; set; }
             public HttpMethod Method { get; set; } = HttpMethod.Get;
             public Dictionary<string, string> Headers { get; set; } = new();
             public object? Data { get; set; }
-            public string ContentType { get; set; } = "application/json";
+            public PostContentType ContentType { get; set; } = PostContentType.Json;
         }
 
         public class HttpResponse
@@ -175,9 +160,10 @@ namespace MSLX.Core.Utils
                 message.Headers.TryAddWithoutValidation(header.Key, header.Value);
             }
 
-            // 处理请求内容
+            // 处理Post请求内容
             if (request.Method != HttpMethod.Get && request.Data != null)
             {
+                _httpClient.DefaultRequestHeaders.Accept.TryParseAdd(HttpAcceptType(request.ContentType));
                 message.Content = CreateHttpContent(request.ContentType, request.Data);
             }
 
@@ -192,41 +178,28 @@ namespace MSLX.Core.Utils
             }
         }
 
-        private HttpContent CreateHttpContent(string contentType, object data)
+        private string HttpAcceptType(PostContentType contentType)
         {
             return contentType switch
             {
-                "application/json" => new StringContent(
-                    JsonSerializer.Serialize(data),
-                    Encoding.UTF8,
-                    "application/json"),
-
-                "application/x-www-form-urlencoded" => new FormUrlEncodedContent(
-                    (data as Dictionary<string, string>)?.Select(kv => new KeyValuePair<string?, string?>(kv.Key, kv.Value))),
-
-                "multipart/form-data" => CreateMultipartContent(data as Dictionary<string, object>),
-
+                PostContentType.Json => "application/json",
+                PostContentType.FormUrlEncoded => "application/x-www-form-urlencoded",
+                PostContentType.Text => "text/plain",
+                PostContentType.Octet => "application/octet-stream",
                 _ => throw new NotSupportedException($"Content type {contentType} is not supported")
             };
         }
 
-        private HttpContent CreateMultipartContent(Dictionary<string, object> formData)
+        private HttpContent CreateHttpContent(PostContentType contentType, object data)
         {
-            var content = new MultipartFormDataContent();
-            foreach (var item in formData)
+            return contentType switch
             {
-                if (item.Value is FileContent file)
-                {
-                    var fileContent = new StreamContent(file.Stream);
-                    fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse(file.ContentType);
-                    content.Add(fileContent, item.Key, file.FileName);
-                }
-                else
-                {
-                    content.Add(new StringContent(item.Value.ToString()), item.Key);
-                }
-            }
-            return content;
+                PostContentType.Json => new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json"),
+                PostContentType.FormUrlEncoded => new FormUrlEncodedContent(data as Dictionary<string, string> ?? new Dictionary<string, string>()),
+                PostContentType.Text => new StringContent(data as string ?? "", Encoding.UTF8, "text/plain"),
+                PostContentType.Octet => new ByteArrayContent(data as byte[] ?? new byte[0]),
+                _ => throw new NotSupportedException($"Content type {contentType} is not supported")
+            };
         }
 
         private async Task<HttpResponse> CreateHttpResponse(HttpResponseMessage response)
@@ -242,7 +215,7 @@ namespace MSLX.Core.Utils
 
             if (_httpHandler.CookieContainer != null)
             {
-                var cookies = _httpHandler.CookieContainer.GetCookies(new Uri(response.RequestMessage.RequestUri.GetLeftPart(UriPartial.Authority)));
+                var cookies = _httpHandler.CookieContainer.GetCookies(new Uri(response.RequestMessage?.RequestUri?.GetLeftPart(UriPartial.Authority) ?? string.Empty));
                 result.Cookies = cookies.Cast<Cookie>()
                     .ToDictionary(c => c.Name, c => c.Value);
             }
@@ -271,27 +244,12 @@ namespace MSLX.Core.Utils
         }
     }
 
-    // 文件上传辅助类
-    public class FileContent
-    {
-        public Stream Stream { get; }
-        public string FileName { get; }
-        public string ContentType { get; }
-
-        public FileContent(Stream stream, string fileName, string contentType)
-        {
-            Stream = stream;
-            FileName = fileName;
-            ContentType = contentType;
-        }
-    }
-
     // UA管理器
     public class UAManager
     {
         public enum UAType { MSLX, Win, Linux, Mac, Android, IOS }
 
-        private static readonly string _mslxUA = "MSLTeam-MSLX/"+ System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+        private static readonly string _mslxUA = "MSLTeam-MSLX/" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
         private static readonly string _winUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
         private static readonly string _linuxUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
         private static readonly string _macUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
