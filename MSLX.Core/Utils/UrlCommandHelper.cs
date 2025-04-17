7aafde07f5b41089f59e55e0b6540e6bbf6e87be
Waheal <2035582067@qq.com>
2025-04-17 22:55:13 +0800

    怎么可能用ICommand呢（恼

--- MSLX.Core/Utils/UrlCommandHelper.cs
+++ MSLX.Core/Utils/UrlCommandHelper.cs
@@ -1,21 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace MSLX.Core.Utils
 {
     public class UrlCommandHelper
     {
-        // 使用单例模式
         private static readonly UrlCommandHelper _instance = new UrlCommandHelper();
         public static UrlCommandHelper Default => _instance;
-
-        public ICommand OpenUrlCommand { get; } = new RelayCommand<string>(OpenUrl);
+        public IRelayCommand OpenUrlCommand { get; } = new RelayCommand<string>(OpenUrl);
 
         private static void OpenUrl(string? url)
         {
@@ -24,7 +17,6 @@ namespace MSLX.Core.Utils
 
             try
             {
-                // 使用默认浏览器打开链接
                 Process.Start(new ProcessStartInfo
                 {
                     FileName = url,
@@ -33,7 +25,6 @@ namespace MSLX.Core.Utils
             }
             catch (Exception ex)
             {
-                // 可以在这里添加日志记录
                 Debug.WriteLine($"打开链接失败: {ex.Message}");
             }
         }
